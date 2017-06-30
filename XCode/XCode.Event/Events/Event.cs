c9b64f567ba5eee9b2cc2c10bc270050dc7ad388
shenlai <shlai244@163.com>
2017-06-30 18:09:57 +0800

    事件

---
+++ XCode/XCode.Event/Events/Event.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XCode.Events.Events
+{
+    public class Event : IEvent
+    {
+        public DateTime CreateTime { get; set; }
+
+        public object Source { get; set; }
+
+        public Event()
+        {
+            CreateTime = DateTime.Now;
+        }
+    }
+
+
+}
---
+++ XCode/XCode.Event/Events/IEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XCode.Events.Events
+{
+    public interface IEvent
+    {
+        /// <summary>
+        /// 产生领域事件的时间
+        /// </summary>
+        DateTime CreateTime { get; set; }
+
+        /// <summary>
+        /// 产生领域事件的事件源对象
+        /// </summary>
+        object Source { get; set; }
+
+
+    }
+}
